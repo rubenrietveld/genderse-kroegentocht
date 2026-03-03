445c991902021c5bfcad06667bb29cb0223ae3fb
rubenrietveld <rubengdc13@gmail.com>
2026-03-03 10:54:40 +0100

    Add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://gendersekroegentocht.nl",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://gendersekroegentocht.nl/events/biercantus-2026",
+      lastModified: new Date(),
+    },
+  ];
+}
